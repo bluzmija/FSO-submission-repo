00587957e1f32ed1dd874912fa089231ad7fb8e9
bluzmija <172497559+bluzmija@users.noreply.github.com>
2025-07-18 03:14:28 +0200

    Refactored code to use object instead of multiple variables

--- part1/1-1-courseinfo/src/App.jsx
+++ part1/1-1-courseinfo/src/App.jsx
@@ -18,9 +18,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <>
-      <Part part={props.pt1} exercises={props.ex1} />
-      <Part part={props.pt2} exercises={props.ex2} />
-      <Part part={props.pt3} exercises={props.ex3} />
+      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
     </>
   )
 }
@@ -28,27 +28,38 @@ const Content = (props) => {
 const Total = (props) => {
   return (
     <>
-      <p>Number of exercises {props.ex1 + props.ex2 + props.ex3}</p>
+      <p>Number of exercises {props.parts[0].exercises +
+				props.parts[1].exercises + props.parts[2].exercises}</p>
     </>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = "Fundementals of React"
-  const exercises1 = 10
-  const part2 = "Using props to pass data"
-  const exercises2 = 7
-  const part3 = "State of a component"
-  const exercises3 = 14
+	const course = {
+		name: "Half stack application development",
+		parts: [
+			{
+				name: "Fundementals of React",
+				exercises: 10
+			},
+			{
+				name: "Using props to pass data",
+				exercises: 7
+			},
+			{
+				name: "State of a component",
+				exercises: 14
+			}
+		]
+	}
 
-  return (
-    <div>
-      <Header course={course}/>
-      <Content pt1={part1} ex1={exercises1} pt2={part2} ex2={exercises2} pt3={part3} ex3={exercises3} />
-      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3} />
-    </div>
-  )
+	return (
+		<div>
+		<Header course={course.name}/>
+		<Content parts={course.parts}/>
+		<Total parts={course.parts}/> 
+		</div>
+	)
 }
 
 export default App
